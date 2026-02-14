50ef71a1a88f24fcf4af7dac2bf8102fb4cf7253
Shannon Lin <shannonlin24@gmail.com>
2026-02-14 15:56:53 -0500

    Navigation changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 // src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/tactile-coding-blocks-website">
+    <HashRouter basename="/tactile-coding-blocks-website">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
