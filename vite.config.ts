411581c89c8413c59f317d41376c90b517ebcdaf
Shannon Lin <shannonlin24@gmail.com>
2026-02-14 15:47:35 -0500

    Base change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tactile-coding-blocks-website/',   // ← THIS FIXES EVERYTHING
+  base: '/tactile-coding-blocks-website/',
 })
