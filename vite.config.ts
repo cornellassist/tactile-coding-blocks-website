cbadea106061852d8e44118be14943fe3241b64f
Shannon Lin <shannonlin24@gmail.com>
2026-03-18 16:48:27 -0400

    Use repo base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Custom domain is served from the site root.
-  base: '/',
+  base: '/tactile-coding-blocks-website/',
 })
